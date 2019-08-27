72110368bf0d599343aa9b9141d0876c26c0df5a
projectEscape <escaperealityproject@gmail.com>
2019-08-27 22:16:08 +0530

    Longest increasing substring

---
+++ GFG/longest-common-substring.cpp
@@ -0,0 +1,35 @@
+// https://practice.geeksforgeeks.org/problems/longest-common-substring/0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubString(int a, int b, string c, string d, int dp[][105]) {
+    int ans = 0;
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b; j++) {
+            if (c[i - 1] == d[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        string c, d;
+        cin >> c >> d;
+        int dp[105][105];
+        memset(dp, 0, sizeof dp);
+        // cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
+
+        cout << longestCommonSubString(a, b, c, d, dp) << endl;
+    }
+
+    return 0;
+}
