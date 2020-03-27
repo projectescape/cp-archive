cfcb5b9933730c332d51279458c40fc16181dc14
projectEscape <escaperealityproject@gmail.com>
2020-03-27 23:58:54 +0530

    longest-substring-without-repeating-characters, leetcode

---
+++ LC/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        int occ[128] = {0};
+        int ans = 0, curr = 0;
+        if ((s[0]) >= 0 && (s[0]) < 128) {
+            occ[s[0]] += 1;
+        }
+        for (int i = 0, j = 1; i < s.size(); i++) {
+            if ((s[i]) >= 0 && (s[i]) < 128) {
+                curr++;
+            } else {
+                continue;
+            }
+            for (; j < s.size(); j++) {
+                if ((s[j]) >= 0 && (s[j]) < 128) {
+                    occ[s[j]] += 1;
+                } else {
+                    continue;
+                }
+                cout << "i = " << i << ", j = " << j << endl;
+                cout << "occ[" << s[i] << "] = " << occ[s[i]] << ", occ[" << s[j] << "] = " << occ[s[j]] << endl;
+                if (occ[s[j]] == 2) {
+                    occ[s[i]] -= 1;
+                    occ[s[j]] -= 1;
+                    break;
+                } else {
+                    curr++;
+                }
+            }
+            ans = max(ans, curr);
+            curr--;
+            curr--;
+        }
+        ans = max(ans, curr);
+        return ans;
+    }
+};
+
+int main() {
+    cout << "Here" << endl;
+    Solution obj;
+    string s = "a";
+    int ans = obj.lengthOfLongestSubstring(s);
+    cout << "Ans = " << ans << endl;
+}
