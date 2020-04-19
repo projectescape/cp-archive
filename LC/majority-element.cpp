316b1d0482ac5ea8afab45b3911fe7abad11ec1f
aniket <escaperealityproject@gmail.com>
2020-04-19 14:30:31 +0530

    Majority Element, leetcode

---
+++ LC/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int majorityElement(vector<int>& nums) {
+        int ans = nums[0], cnt = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == ans) {
+                cnt++;
+
+            } else {
+                cnt--;
+                if (cnt == 0) {
+                    cnt = 1;
+                    ans = nums[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
