d3bc6c268f4aca921f63609c9b3dcbd7cd726304
projectescape <escaperealityproject@gmail.com>
2020-06-20 15:17:12 +0530

    Rotated Array, interviewbit

---
+++ IB/rotated-array.cpp
@@ -0,0 +1,15 @@
+int Solution::findMin(const vector<int> &A) {
+    int n = A.size();
+    int low = 0;
+    int high = n - 1;
+    int index = 0;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (A[mid] < A[index]) {
+            index = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+        return A[index];
+    }
