04285ba294b04c73bc9e6adc29c2cb49bb4e03df
projectescape <escaperealityproject@gmail.com>
2020-06-20 22:45:30 +0530

    Longest increasing subsequence, leetcode

---
+++ LC/longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[j] >= dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
--- LC/queue-reconstruction-by-height.cpp
+++ LC/queue-reconstruction-by-height.cpp
@@ -1,37 +1,25 @@
+bool comp(vector<int>& a, vector<int>& b) {
+    if (a[0] != b[0]) return a[0] < b[0];
+    return a[1] < b[1];
+}
+
 class Solution {
    public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b) {
-            if (a[0] == b[0]) return a[1] < b[1];
-            return a[0] < b[0];
-        });
-
-        vector<vector<int>> ans(people.size(), vector<int>(2, -1));
-        int count, i;
-        for (auto p : people) {
-            count = p[1];
-            for (i = 0;;) {
-                if (count == 0) {
-                    if (ans[i][0] == -1) {
-                        ans[i] = p;
-                        break;
-                    } else {
-                        i++;
-                        continue;
-                    }
-                } else {
-                    if (ans[i][0] == -1) {
-                        count--;
-                        i++;
-
-                    } else if (ans[i][0] >= p[0]) {
-                        count--;
-                        i++;
-                    } else {
-                        i++;
-                    }
+        sort(people.begin(), people.end(), comp);
+        vector<vector<int>> ans(people.size());
+        int temp, j;
+        for (auto i : people) {
+            // cout<<"["<<i[0]<<","<<i[1]<<"], ";
+            for (j = 0, temp = i[1]; temp > 0;) {
+                if (ans[j].size() == 0 || ans[j][0] >= i[0]) {
+                    temp--;
                 }
+                j++;
             }
+            while (ans[j].size() != 0) j++;
+            // cout<<"j :"<<j<<", ";
+            ans[j] = i;
         }
         return ans;
     }
