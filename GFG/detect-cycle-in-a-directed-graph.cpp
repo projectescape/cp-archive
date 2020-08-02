b6da2e356c2c4b55f8a4371174fb1384e4347fc2
projectescape <escaperealityproject@gmail.com>
2020-08-02 12:36:25 +0530

    Detect Cycle in a Directed graph, GFG

---
+++ GFG/detect-cycle-in-a-directed-graph.cpp
@@ -0,0 +1,28 @@
+bool isCyclic(int V, vector<int> adj[]) {
+    int temp;
+    vector<int> deg(V, 0);
+    for (int i = 0; i < V; i++) {
+        for (auto j : adj[i]) {
+            deg[j]++;
+        }
+    }
+    queue<int> q;
+    for (int i = 0; i < V; i++) {
+        if (deg[i] == 0) {
+            q.push(i);
+        }
+    }
+    vector<int> ans;
+    while (!q.empty()) {
+        temp = q.front();
+        q.pop();
+        ans.push_back(temp);
+        for (auto i : adj[temp]) {
+            deg[i]--;
+            if (deg[i] == 0) {
+                q.push(i);
+            }
+        }
+    }
+    return ans.size() != V;
+}
