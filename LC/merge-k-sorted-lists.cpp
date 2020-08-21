effbcca8d5342740b4ee33c122b441be184e343c
projectescape <escaperealityproject@gmail.com>
2020-08-21 18:45:16 +0530

    Merge K sorted lists, leetcode, improved

--- LC/merge-k-sorted-lists.cpp
+++ LC/merge-k-sorted-lists.cpp
@@ -1,39 +1,71 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+class comp {
+   public:
+    bool operator()(ListNode*& a, ListNode*& b) {
+        return a->val > b->val;
+    }
+};
+
 class Solution {
    public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        stack<ListNode*> s;
-        int comp, index;
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
         for (auto i : lists) {
-            if (i == NULL) n--;
+            if (i)
+                pq.push(i);
         }
-        while (n > 0) {
-            comp = INT_MAX;
-            for (int i = 0; i < lists.size(); i++) {
-                if (lists[i] == NULL) continue;
-                if (lists[i]->val < comp) {
-                    comp = lists[i]->val;
-                    index = i;
-                }
+        if (pq.empty()) return NULL;
+        ListNode *ans = pq.top(), *temp = pq.top();
+        pq.pop();
+        if (temp->next) pq.push(temp->next);
+        while (!pq.empty()) {
+            temp->next = pq.top();
+            temp = temp->next;
+            pq.pop();
+            if (temp->next) {
+                pq.push(temp->next);
             }
-            s.push(lists[index]);
-            lists[index] = lists[index]->next;
-            if (lists[index] == NULL) n--;
         }
-        ListNode* head = NULL;
-        while (!s.empty()) {
-            s.top()->next = head;
-            head = s.top();
-            s.pop();
-        }
-        return head;
+        temp->next = NULL;
+        return ans;
     }
 };
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+//    public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         int n = lists.size();
+//         stack<ListNode*> s;
+//         int comp, index;
+//         for (auto i : lists) {
+//             if (i == NULL) n--;
+//         }
+//         while (n > 0) {
+//             comp = INT_MAX;
+//             for (int i = 0; i < lists.size(); i++) {
+//                 if (lists[i] == NULL) continue;
+//                 if (lists[i]->val < comp) {
+//                     comp = lists[i]->val;
+//                     index = i;
+//                 }
+//             }
+//             s.push(lists[index]);
+//             lists[index] = lists[index]->next;
+//             if (lists[index] == NULL) n--;
+//         }
+//         ListNode* head = NULL;
+//         while (!s.empty()) {
+//             s.top()->next = head;
+//             head = s.top();
+//             s.pop();
+//         }
+//         return head;
+//     }
+// };
