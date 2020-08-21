de24321a43003074bfc2210d40dd554f977901cf
projectescape <escaperealityproject@gmail.com>
2020-08-21 18:57:05 +0530

    Kth largest element in an array, leetcode

---
+++ LC/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,20 @@
+class comp {
+   public:
+    bool operator()(int& a, int& b) {
+        return a > b;
+    }
+};
+
+class Solution {
+   public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, comp> pq;
+        for (auto i : nums) {
+            pq.push(i);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
