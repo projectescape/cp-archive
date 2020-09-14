2c946a037792ce1b32ec27a4c492e3153da691da
projectescape <escaperealityproject@gmail.com>
2020-09-14 12:29:53 +0530

    Lowest common ancestor in a binary tree, GFG

---
+++ GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -0,0 +1,8 @@
+Node* lca(Node* root, int n1, int n2) {
+    if (!root) return NULL;
+    if (root->data == n1 || root->data == n2) return root;
+    Node *l = lca(root->left, n1, n2), *r = lca(root->right, n1, n2);
+    if (l && r) return root;
+    if (r) return r;
+    return l;
+}
