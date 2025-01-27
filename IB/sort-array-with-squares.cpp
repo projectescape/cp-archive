7cb26fa2bbd054567f55b12b667b98adb854502a
Aniket <escaperealityproject@gmail.com>
2025-01-27 20:40:45 +0530

    Modify IB/sort-array-with-squares.cpp to be solved in O(n)

--- IB/sort-array-with-squares.cpp
+++ IB/sort-array-with-squares.cpp
@@ -1,10 +1,59 @@
-bool comp(int &a, int &b) { return a < b; }
+vector<int> absoluteSortedArray(int midIndex, vector<int> &A)
+{
 
-vector<int> Solution::solve(vector<int> &A) {
-    if (A.size() == 0) return {};
-    for (int i = 0; i < A.size(); i++) {
-        A[i] *= A[i];
+    vector<int> answer;
+
+    int i = midIndex - 1, j = midIndex;
+
+    for (; i >= 0 && j < A.size();)
+    {
+        if (abs(A[i]) < A[j])
+        {
+            answer.push_back(A[i]);
+            i--;
+        }
+        else
+        {
+            answer.push_back(A[j]);
+            j++;
+        }
     }
-    sort(A.begin(), A.end(), comp);
-    return A;
+
+    if (i >= 0)
+    {
+        for (; i >= 0; i--)
+        {
+            answer.push_back(A[i]);
+        }
+    }
+    else
+    {
+        for (; j < A.size(); j++)
+        {
+            answer.push_back(A[j]);
+        }
+    }
+
+    return answer;
+}
+
+vector<int> Solution::solve(vector<int> &A)
+{
+    int firstPositiveNumberIndex = A.size();
+    for (int index = 0; index < A.size(); index++)
+    {
+        if (A[index] >= 0)
+        {
+            firstPositiveNumberIndex = index;
+            break;
+        }
+    }
+
+    vector<int> answer = absoluteSortedArray(firstPositiveNumberIndex, A);
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        answer[i] = answer[i] * answer[i];
+    }
+    return answer;
 }
