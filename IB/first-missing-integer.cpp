8b3c90002b8fe92674370fedee4c7728897589bd
Aniket <escaperealityproject@gmail.com>
2025-01-28 21:28:50 +0530

    Update First missing integer, IB

--- IB/first-missing-integer.cpp
+++ IB/first-missing-integer.cpp
@@ -1,54 +1,32 @@
-int Solution::firstMissingPositive(vector<int> &A) {
-    int i;
-    for (i = 0; i < A.size(); i++) {
-        if (A[i] > A.size() || A[i] <= 0) {
-            A[i] = A.size() + 1;
+int Solution::firstMissingPositive(vector<int> &A)
+{
+    int answer = A.size() + 1;
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] <= 0)
+        {
+            A[i] = INT_MAX;
         }
     }
-    for (i = 0; i < A.size(); i++) {
-        if (abs(A[i]) <= A.size()) {
-            A[abs(A[i]) - 1] = -1 * abs(A[abs(A[i]) - 1]);
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        int absValue = abs(A[i]);
+        if (absValue <= A.size())
+        {
+            A[absValue - 1] = abs(A[absValue - 1]) * -1;
         }
     }
-    for (i = 0; i < A.size(); i++) {
-        if (A[i] > 0) return i + 1;
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] > 0)
+        {
+            answer = i + 1;
+            break;
+        }
     }
-    return A.size() + 1;
+
+    return answer;
 }
-
-// #include <bits/stdc++.h>
-// #include <iostream>
-// using namespace std;
-
-// int firstMissingPositive(vector<int> &A) {
-//     long long size = A.size(), negIndex = A.size() - 1;
-//     // Initialize new array with all values 1
-//     for (long long i = 0; i < size; i++) {
-//         if (A[i] < size + 1 && A[i] > 0) {
-//             long long temp = 0, j = i;
-//             while (1) {
-//                 cout << "temp " << temp << " i " << i << " j " << j << endl;
-//                 temp = A[A[i] - 1];
-//                 A[A[i] - 1] = size;
-//                 if (temp - 1 == i) {
-//                     break;
-//                 }
-//                 if (A[temp] <= size) {
-//                     i = temp;
-//                 } else {
-//                     break;
-//                 }
-//             }
-//             i = j;
-//         }
-//     }
-//     for (long long i = 0; i < size; i++) {
-//         if (A[i] != size) {
-//             return i;
-//         }
-//     }
-// }
-// int main() {
-//     vector<int> A = {1, 3, -1, 4};
-//     cout << firstMissingPositive(A) << endl;
-// }
