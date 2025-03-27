3a5f2d8337f2c9566eded1accfde353eb5070251
aniket <escaperealityproject@gmail.com>
2025-03-27 19:17:10 +0530

    Power of 2, IB

--- IB/power-of-2.cpp
+++ IB/power-of-2.cpp
@@ -1,24 +1,30 @@
-void mult(string &s, int n) {
-    int carry = 0, temp;
-    for (int i = 0; i < s.size(); i++) {
-        temp = (s[i] - '0') * 2 + carry;
-        carry = 0;
-        if (temp > 9) {
-            carry = 1;
-            temp -= 10;
-        }
-        s[i] = temp + '0';
+int divideBy2(string A)
+{
+    if (A.size() == 1 && A[0] == '2')
+    {
+        return 1;
     }
-    if (carry) s.push_back('1');
+    int curr = 0;
+    string s;
+    for (int i = 0; (i < A.size()); i++)
+    {
+        curr = curr * 10 + A[i] - '0';
+        if (!((s.size() == 0) && (curr / 2 == 0)))
+        {
+            s += (curr / 2 + '0');
+        }
+        curr = curr % 2;
+    }
+    if (curr != 0)
+        return 0;
+    return divideBy2(s);
 }
 
-int Solution::power(string A) {
-    if (A == "0") return 1;
-    reverse(A.begin(), A.end());
-    string temp = "2";
-    while (temp.size() <= A.size()) {
-        if (temp == A) return 1;
-        mult(temp, 2);
-    }
-    return 0;
+int Solution::power(string A)
+{
+    if (A.size() == 0)
+        return 0;
+    if (A.size() == 1 && (A[0] == '0'))
+        return 0;
+    return divideBy2(A);
 }
