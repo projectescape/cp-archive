db94ff770f55dab896c50547a85e5e708fd683d6
aniket <escaperealityproject@gmail.com>
2025-07-03 21:26:31 +0530

    Buy and sell crypto, NC

--- NC/buy-and-sell-crypto.py
+++ NC/buy-and-sell-crypto.py
@@ -15,3 +15,19 @@ class Solution:
             ans = max(ans, maxAtRightFromPos[i]-prices[i])
 
         return ans
+
+
+class SolutionWithBetterSpaceComplexity:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l, r = 0, 1
+        ans = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                ans = max(ans, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+
+        return ans
