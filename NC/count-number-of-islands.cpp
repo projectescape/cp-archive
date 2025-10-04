95739ecf82f7d90002c57008babb953a9c0e0665
aniket <escaperealityproject@gmail.com>
2025-10-04 18:36:44 +0530

    Count number of islands, NC

---
+++ NC/count-number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    void traverseIsland(vector<vector<char>> &grid, int i, int j)
+    {
+        if (i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] != '1')
+        {
+            return;
+        }
+        grid[i][j] = '2';
+        traverseIsland(grid, i + 1, j);
+        traverseIsland(grid, i - 1, j);
+        traverseIsland(grid, i, j + 1);
+        traverseIsland(grid, i, j - 1);
+    }
+
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int count = 0;
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    cout << "Found island at " << i << ", " << j << endl;
+                    count++;
+                    traverseIsland(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
