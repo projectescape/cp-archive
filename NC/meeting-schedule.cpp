567859aeade4e177bea612952499efceaaaa8c96
aniket <escaperealityproject@gmail.com>
2025-11-16 16:19:43 +0530

    Meeting rooms, NC

---
+++ NC/meeting-schedule.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution
+{
+public:
+    bool canAttendMeetings(vector<Interval> &intervals)
+    {
+        sort(intervals.begin(), intervals.end(), [](Interval &a, Interval &b)
+             { return a.start < b.start; });
+
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            Interval curr = intervals[i];
+            Interval prev = intervals[i - 1];
+
+            if (curr.start < prev.end)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
