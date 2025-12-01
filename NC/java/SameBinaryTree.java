7a21b7fed9e65980cbebbb3f1f9c482e43b81fd8
aniket <escaperealityproject@gmail.com>
2025-12-01 17:51:08 +0530

    Same Binary Tree, NC

---
+++ NC/java/SameBinaryTree.java
@@ -0,0 +1,16 @@
+package NC.java;
+
+import javax.swing.tree.TreeNode;
+
+public class SameBinaryTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
