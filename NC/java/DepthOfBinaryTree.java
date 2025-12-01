a2f0b65a263cc5166b1579f202cace3c19712f64
aniket <escaperealityproject@gmail.com>
2025-12-01 17:49:25 +0530

    Maximum Depth of Binary Tree, NC

---
+++ NC/java/DepthOfBinaryTree.java
@@ -0,0 +1,14 @@
+package NC.java;
+
+import javax.swing.tree.TreeNode;
+
+public class DepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+}
